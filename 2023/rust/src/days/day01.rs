e26caf4bfd170946a9fb0c9f77f121c75858038e
Julian Doherty <julian@juliandoherty.com>
2023-12-01 18:13:14 +1100

    cleaned up day 1

--- 2023/rust/src/days/day01.rs
+++ 2023/rust/src/days/day01.rs
@@ -1,22 +1,20 @@
 pub fn part1(data: String) -> String {
     data.trim()
-        .split("\n")
-        .map(first_last_digit)
-        .map(|(n1, n2)| n1 * 10 + n2)
+        .lines()
+        .map(calibration)
         .sum::<u32>()
         .to_string()
 }
 
 pub fn part2(data: String) -> String {
     data.trim()
-        .split("\n")
-        .map(first_last_digit_words)
-        .map(|(n1, n2)| n1 * 10 + n2)
+        .lines()
+        .map(calibration2)
         .sum::<u32>()
         .to_string()
 }
 
-fn first_last_digit(line: &str) -> (u32, u32) {
+fn calibration(line: &str) -> u32 {
     let n1 = line
         .chars()
         .find(|c| c.is_digit(10))
@@ -31,58 +29,35 @@ fn first_last_digit(line: &str) -> (u32, u32) {
         .to_digit(10)
         .unwrap();
 
-    (n1, n2)
+    n1 * 10 + n2
 }
 
-fn first_last_digit_words(line: &str) -> (u32, u32) {
-    let mut n1 = 0;
-    for i in 0..line.len() {
-        let n = parse_digit_or_word_start(&line[i..]);
-        if n.is_some() {
-            n1 = n.unwrap();
-            break;
-        }
-    }
+fn calibration2(line: &str) -> u32 {
+    let n1 = line
+        .char_indices()
+        .find_map(|(i, _c)| digit_or_number(&line[i..], |s, start| s.starts_with(start)))
+        .unwrap();
 
-    let mut n2 = 0;
-    for i in 0..line.len() {
-        let n = parse_digit_or_word_end(&line[0..line.len() - i]);
-        if n.is_some() {
-            n2 = n.unwrap();
-            break;
-        }
-    }
-    (n1, n2)
+    let n2 = line
+        .char_indices()
+        .rev()
+        .find_map(|(i, _c)| digit_or_number(&line[..=i], |s, start| s.ends_with(start)))
+        .unwrap();
+    n1 * 10 + n2
 }
 
-fn parse_digit_or_word_start(s: &str) -> Option<u32> {
+fn digit_or_number(s: &str, f: impl Fn(&str, &str) -> bool) -> Option<u32> {
     match s {
-        s if s.starts_with("zero") || s.starts_with("0") => Some(0),
-        s if s.starts_with("one") || s.starts_with("1") => Some(1),
-        s if s.starts_with("two") || s.starts_with("2") => Some(2),
-        s if s.starts_with("three") || s.starts_with("3") => Some(3),
-        s if s.starts_with("four") || s.starts_with("4") => Some(4),
-        s if s.starts_with("five") || s.starts_with("5") => Some(5),
-        s if s.starts_with("six") || s.starts_with("6") => Some(6),
-        s if s.starts_with("seven") || s.starts_with("7") => Some(7),
-        s if s.starts_with("eight") || s.starts_with("8") => Some(8),
-        s if s.starts_with("nine") || s.starts_with("9") => Some(9),
-        &_ => None,
-    }
-}
-
-fn parse_digit_or_word_end(s: &str) -> Option<u32> {
-    match s {
-        s if s.ends_with("zero") || s.ends_with("0") => Some(0),
-        s if s.ends_with("one") || s.ends_with("1") => Some(1),
-        s if s.ends_with("two") || s.ends_with("2") => Some(2),
-        s if s.ends_with("three") || s.ends_with("3") => Some(3),
-        s if s.ends_with("four") || s.ends_with("4") => Some(4),
-        s if s.ends_with("five") || s.ends_with("5") => Some(5),
-        s if s.ends_with("six") || s.ends_with("6") => Some(6),
-        s if s.ends_with("seven") || s.ends_with("7") => Some(7),
-        s if s.ends_with("eight") || s.ends_with("8") => Some(8),
-        s if s.ends_with("nine") || s.ends_with("9") => Some(9),
+        s if f(s, "zero") || f(s, "0") => Some(0),
+        s if f(s, "one") || f(s, "1") => Some(1),
+        s if f(s, "two") || f(s, "2") => Some(2),
+        s if f(s, "three") || f(s, "3") => Some(3),
+        s if f(s, "four") || f(s, "4") => Some(4),
+        s if f(s, "five") || f(s, "5") => Some(5),
+        s if f(s, "six") || f(s, "6") => Some(6),
+        s if f(s, "seven") || f(s, "7") => Some(7),
+        s if f(s, "eight") || f(s, "8") => Some(8),
+        s if f(s, "nine") || f(s, "9") => Some(9),
         &_ => None,
     }
 }
