9d5413c08129ca2b51d826dcbd1f0ac39f1c5fa9
Julian Doherty <julian@juliandoherty.com>
2023-12-11 00:44:11 +1100

    day 8 WIP tests pass, real input hangs

--- 2023/rust/src/days/day08.rs
+++ 2023/rust/src/days/day08.rs
@@ -1,13 +1,13 @@
 use nom::{
     branch::alt,
     bytes::complete::tag,
-    character::complete::{alpha1, multispace0, multispace1},
+    character::complete::{alphanumeric1, multispace0, multispace1},
     combinator::{map, value, verify},
     multi::{fold_many0, many0},
     sequence::{delimited, separated_pair, terminated},
     IResult,
 };
-use std::{collections::HashMap, iter::Cycle};
+use std::collections::HashMap;
 
 use crate::ParseError;
 
@@ -15,26 +15,35 @@ pub fn part1(data: &str) -> Result<String, Box<dyn std::error::Error>> {
     let (instructions, nodes) = parse(data)?;
 
     Ok(nodes
-        .iter("AAA", "ZZZ", instructions.into_iter())
+        .iter("AAA", |node| node.id == "ZZZ", &instructions)
         .count()
         .to_string())
 }
 
-pub fn part2(_data: &str) -> Result<String, Box<dyn std::error::Error>> {
-    todo!()
+pub fn part2(data: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let (instructions, nodes) = parse(data)?;
+
+    Ok(nodes
+        .ghost_iter("A", |node| node.id.ends_with("Z"), &instructions)
+        .enumerate()
+        .inspect(|nodes| {
+            dbg!(&nodes);
+        })
+        .count()
+        .to_string())
 }
 
-#[derive(Clone)]
+#[derive(Clone, Copy, Debug)]
 enum Instruction {
     Left,
     Right,
 }
 
+type NodeId<'a> = &'a str;
+
 struct Nodes<'a> {
     node_lookup: HashMap<NodeId<'a>, Node<'a>>,
 }
-
-type NodeId<'a> = &'a str;
 
 impl<'a> Nodes<'a> {
     fn new() -> Self {
@@ -54,13 +63,121 @@ impl<'a> Nodes<'a> {
     fn iter(
         &'a self,
         start_node_id: NodeId<'a>,
-        finish_node_id: NodeId<'a>,
-        instructions: impl Iterator<Item = Instruction> + Clone + 'a,
+        finished: fn(&Node) -> bool,
+        instructions: &'a Vec<Instruction>,
     ) -> impl Iterator<Item = &'a Node> + 'a {
-        NodesIterator::new(start_node_id, finish_node_id, instructions, &self)
+        NodesIterator::new(start_node_id, finished, &instructions, &self)
+    }
+
+    fn ghost_iter(
+        &'a self,
+        start_suffix: &'a str,
+        finished: fn(&Node) -> bool,
+        instructions: &'a Vec<Instruction>,
+    ) -> impl Iterator<Item = Vec<&'a Node>> + 'a {
+        let start_nodes = self
+            .node_lookup
+            .keys()
+            .filter(|node_id| node_id.ends_with(start_suffix))
+            .collect::<Vec<_>>();
+
+        GhostNodesIter::new(start_nodes, finished, instructions, &self)
     }
 }
 
+struct NodesIterator<'a> {
+    current_node: &'a Node<'a>,
+    finished: fn(&Node) -> bool,
+    instructions: Box<dyn Iterator<Item = Instruction> + 'a>,
+    nodes: &'a Nodes<'a>,
+}
+
+impl<'a> NodesIterator<'a> {
+    fn new(
+        start_node_id: NodeId<'a>,
+        finished: fn(&Node) -> bool,
+        instructions: &'a Vec<Instruction>,
+        nodes: &'a Nodes<'a>,
+    ) -> Self {
+        Self {
+            current_node: nodes.get(start_node_id),
+            finished,
+            instructions: Box::new(instructions.iter().copied().cycle()),
+            nodes,
+        }
+    }
+}
+
+impl<'a> Iterator for NodesIterator<'a> {
+    type Item = &'a Node<'a>;
+    fn next(&mut self) -> Option<Self::Item> {
+        if (self.finished)(self.current_node) {
+            return None;
+        }
+
+        let instruction = self.instructions.next().unwrap();
+        let next_node_id = self.current_node.next_node_id(instruction);
+
+        let next_node = self.nodes.get(next_node_id);
+        self.current_node = next_node;
+
+        Some(next_node)
+    }
+}
+
+struct GhostNodesIter<'a> {
+    nodes_iterators: Option<Vec<NodesIterator<'a>>>,
+    finished: fn(&Node) -> bool,
+}
+
+impl<'a> GhostNodesIter<'a> {
+    fn new(
+        start_nodes: Vec<&'a NodeId>,
+        finished: fn(&Node) -> bool,
+        instructions: &'a Vec<Instruction>,
+        nodes: &'a Nodes,
+    ) -> Self {
+        GhostNodesIter {
+            nodes_iterators: start_nodes
+                .iter()
+                .map(|start_node_id| {
+                    Some(NodesIterator::new(
+                        start_node_id,
+                        |_node| false,
+                        instructions,
+                        nodes,
+                    ))
+                })
+                .collect(),
+            finished,
+        }
+    }
+}
+
+impl<'a> Iterator for GhostNodesIter<'a> {
+    type Item = Vec<&'a Node<'a>>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.nodes_iterators.is_none() {
+            return None;
+        }
+
+        let ghost_nodes: Vec<_> = self
+            .nodes_iterators
+            .as_mut()
+            .unwrap()
+            .iter_mut()
+            .map(|node_iter| node_iter.next().unwrap())
+            .collect();
+
+        if ghost_nodes.iter().all(|node| (self.finished)(node)) {
+            self.nodes_iterators = None
+        }
+        Some(ghost_nodes)
+    }
+}
+
+#[derive(Debug)]
 struct Node<'a> {
     id: NodeId<'a>,
     left: NodeId<'a>,
@@ -77,52 +194,6 @@ impl<'a> Node<'a> {
             Instruction::Left => &self.left,
             Instruction::Right => &self.right,
         }
-    }
-}
-
-struct NodesIterator<'a, I> {
-    current_node: &'a Node<'a>,
-    finish_node_id: NodeId<'a>,
-    instructions: Cycle<I>,
-    nodes: &'a Nodes<'a>,
-}
-
-impl<'a, I> NodesIterator<'a, I>
-where
-    I: Iterator<Item = Instruction> + Clone,
-{
-    fn new(
-        start_node_id: NodeId<'a>,
-        finish_node_id: NodeId<'a>,
-        instructions: I,
-        nodes: &'a Nodes,
-    ) -> Self {
-        Self {
-            current_node: nodes.get(start_node_id),
-            finish_node_id,
-            instructions: instructions.cycle(),
-            nodes,
-        }
-    }
-}
-
-impl<'a, I> Iterator for NodesIterator<'a, I>
-where
-    I: Iterator<Item = Instruction> + Clone,
-{
-    type Item = &'a Node<'a>;
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.current_node.id == self.finish_node_id {
-            return None;
-        }
-
-        let instruction = self.instructions.next().unwrap();
-        let next_node_id = self.current_node.next_node_id(instruction);
-
-        let next_node = self.nodes.get(next_node_id);
-        self.current_node = next_node;
-
-        Some(next_node)
     }
 }
 
@@ -176,5 +247,5 @@ fn node(s: &str) -> IResult<&str, Node> {
 }
 
 fn node_id(s: &str) -> IResult<&str, NodeId> {
-    verify(alpha1, |chars: &str| chars.len() == 3)(s)
+    verify(alphanumeric1, |chars: &str| chars.len() == 3)(s)
 }
--- 2023/rust/tests/day08_test.rs
+++ 2023/rust/tests/day08_test.rs
@@ -1,6 +1,6 @@
 use aoc2023::days::day08::{part1, part2};
 
-const DATA: &str = "\
+const DATA1: &str = "\
 RL
 
 AAA = (BBB, CCC)
@@ -11,23 +11,35 @@ EEE = (EEE, EEE)
 GGG = (GGG, GGG)
 ZZZ = (ZZZ, ZZZ)";
 
-const DATA2: &str = "\
+const DATA1_1: &str = "\
 LLR
 
 AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)";
 
+const DATA2: &str = "\
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)";
+
 #[test]
 fn part1_example_data() {
-    assert_eq!(part1(DATA).map_err(|e| e.to_string()), Ok("2".to_string()));
-    assert_eq!(part1(DATA2).map_err(|e| e.to_string()), Ok("6".to_string()));
+    assert_eq!(part1(DATA1).map_err(|e| e.to_string()), Ok("2".to_string()));
+    assert_eq!(
+        part1(DATA1_1).map_err(|e| e.to_string()),
+        Ok("6".to_string())
+    );
 }
 
 #[test]
 fn part2_example_data() {
-    assert_eq!(
-        part2(DATA).map_err(|e| e.to_string()),
-        Ok("part2 TODO".to_string())
-    );
+    assert_eq!(part2(DATA2).map_err(|e| e.to_string()), Ok("6".to_string()));
 }
