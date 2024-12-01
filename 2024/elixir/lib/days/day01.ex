8c514ee46cfed291f7e83a42e933c5e8f14d2530
Julian Doherty <julian@juliandoherty.com>
2024-12-01 21:07:22 +1100

    day 1

---
+++ 2024/elixir/lib/days/day01.ex
@@ -0,0 +1,34 @@
+defmodule Aoc24.Days.Day01 do
+  def part1(input) do
+    input
+    |> parse()
+    |> then(fn {ns1, ns2} -> [Enum.sort(ns1), Enum.sort(ns2)] end)
+    |> Enum.zip_with(fn [n1, n2] -> abs(n2 - n1) end)
+    |> Enum.sum()
+  end
+
+  def part2(input) do
+    {ns1, ns2} =
+      input
+      |> parse()
+
+    ns2_counts =
+      ns2
+      |> Enum.reduce(%{}, fn n, counts -> Map.update(counts, n, 1, &(&1 + 1)) end)
+
+    ns1
+    |> Enum.map(fn n1 -> n1 * Map.get(ns2_counts, n1, 0) end)
+    |> Enum.sum()
+  end
+
+  defp parse(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(fn line ->
+      [n1, n2] = line |> String.split(~r/\s+/)
+      {String.to_integer(n1), String.to_integer(n2)}
+    end)
+    |> Enum.reduce({[], []}, fn {n1, n2}, {ns1, ns2} -> {[n1 | ns1], [n2 | ns2]} end)
+  end
+end
---
+++ 2024/elixir/test/days/day01_test.exs
@@ -0,0 +1,31 @@
+defmodule Aoc24.Days.Day01Test do
+  use ExUnit.Case, async: true
+
+  alias Aoc24.Days.Day01
+  doctest Day01
+
+  setup do
+    example = """
+    3   4
+    4   3
+    2   5
+    1   3
+    3   9
+    3   3
+    """
+
+    {:ok, example: example}
+  end
+
+  describe "part 1" do
+    test "part 1", ctx do
+      assert Day01.part1(ctx.example) == 11
+    end
+  end
+
+  describe "part 2" do
+    test "part 2", ctx do
+      assert Day01.part2(ctx.example) == 31
+    end
+  end
+end
