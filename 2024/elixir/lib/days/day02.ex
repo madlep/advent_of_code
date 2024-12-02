e28bb9f271f299e328bda022616a10b14e2e43da
Julian Doherty <julian@juliandoherty.com>
2024-12-02 18:28:03 +1100

    rename

--- 2024/elixir/lib/days/day02.ex
+++ 2024/elixir/lib/days/day02.ex
@@ -4,8 +4,7 @@ defmodule Aoc24.Days.Day02 do
     input
     |> Enum.count(fn line ->
       line
-      |> String.split()
-      |> Enum.map(&String.to_integer/1)
+      |> parse_report()
       |> safe?()
     end)
   end
@@ -15,12 +14,13 @@ defmodule Aoc24.Days.Day02 do
     input
     |> Enum.count(fn line ->
       line
-      |> String.split()
-      |> Enum.map(&String.to_integer/1)
-      |> take_one_perms()
+      |> parse_report()
+      |> tolerated_permutations()
       |> Enum.any?(&safe?/1)
     end)
   end
+
+  defp parse_report(line), do: line |> String.split() |> Enum.map(&String.to_integer/1)
 
   defp tolerated([r1, r2 | _]), do: if(r1 < r2, do: 1..3, else: -3..-1)
 
@@ -28,8 +28,8 @@ defmodule Aoc24.Days.Day02 do
   defp safe?([_r], _), do: true
   defp safe?([r1 | [r2 | _] = rest], t), do: if((r2 - r1) in t, do: safe?(rest, t), else: false)
 
-  defp take_one_perms(reports), do: t1perms(reports, [], [])
+  defp tolerated_permutations(reports), do: perms(reports, [], [])
 
-  defp t1perms([], _, acc), do: Enum.reverse(acc)
-  defp t1perms([r | rs], rs2, acc), do: t1perms(rs, [r | rs2], [Enum.reverse(rs2) ++ rs | acc])
+  defp perms([], _, acc), do: Enum.reverse(acc)
+  defp perms([r | rs], rs2, acc), do: perms(rs, [r | rs2], [Enum.reverse(rs2) ++ rs | acc])
 end
