91c46d84f17b3bc45075f2d2c641ee60da021f56
Julian Doherty <julian@juliandoherty.com>
2024-12-11 18:52:30 +1100

    clean up part 1

--- 2024/elixir/lib/aoc24/day10.ex
+++ 2024/elixir/lib/aoc24/day10.ex
@@ -9,76 +9,47 @@ defmodule Aoc24.Day10 do
         reduce_with: {fn {_, element}, acc -> {:keep, String.to_integer(element), acc} end, nil}
       )
 
-    memo =
+    {_, total_score} =
       for x <- Dense.xs(map),
           y <- Dense.ys(map),
-          reduce: %{} do
-        memo ->
-          walk({x, y}, map, memo)
+          reduce: {%{}, 0} do
+        {memo, total_score} ->
+          if Dense.at(map, {x, y}) == 0 do
+            memo = peaks(memo, {x, y}, map)
+            {memo, total_score + MapSet.size(memo[{x, y}])}
+          else
+            {memo, total_score}
+          end
       end
 
-    for x <- Dense.xs(map),
-        y <- Dense.ys(map),
-        reduce: 0 do
-      total_score ->
-        if Dense.at(map, {x, y}) == 0 do
-          (memo[{x, y}] |> MapSet.size()) + total_score
-        else
-          total_score
-        end
-    end
+    total_score
   end
 
-  @neighbour_dirs [
-    {-1, 0},
-    {0, -1},
-    {1, 0},
-    {0, 1}
-  ]
+  @neighbour_dirs [{-1, 0}, {0, -1}, {1, 0}, {0, 1}]
 
   @spec part2(String.t()) :: integer()
   def part2(_input) do
     -1
   end
 
-  defp walk(position, _map, memo) when is_map_key(memo, position), do: memo
+  defp peaks(memo, pos, _map) when is_map_key(memo, pos), do: memo
 
-  defp walk({x, y} = position, map, memo) do
-    case Dense.at(map, position) do
+  defp peaks(memo, {x, y} = pos, map) do
+    case Dense.at(map, pos) do
       nil ->
         memo
 
       9 ->
-        Map.put(memo, position, MapSet.new([position]))
+        Map.put(memo, pos, MapSet.new([pos]))
 
       n when n in 0..8 ->
-        {new_memo, result} =
-          @neighbour_dirs
-          |> Enum.map(fn {dx, dy} -> {x + dx, y + dy} end)
-          |> Enum.filter(&(Dense.at(map, &1) == n + 1))
-          |> Enum.reduce({memo, MapSet.new()}, fn neighbour_position, {memo_acc, result_acc} ->
-            new_memo_acc = walk(neighbour_position, map, memo_acc)
-            new_result_acc = MapSet.union(result_acc, new_memo_acc[neighbour_position])
-            {new_memo_acc, new_result_acc}
-          end)
-
-        Map.put(new_memo, position, result)
+        @neighbour_dirs
+        |> Enum.map(fn {dx, dy} -> {x + dx, y + dy} end)
+        |> Enum.filter(&(Dense.at(map, &1) == n + 1))
+        |> Enum.reduce(Map.put(memo, pos, MapSet.new()), fn neighbour_pos, memo ->
+          memo = peaks(memo, neighbour_pos, map)
+          Map.update!(memo, pos, fn old_memo -> MapSet.union(old_memo, memo[neighbour_pos]) end)
+        end)
     end
-  end
-
-  defp print(map, memo) do
-    IO.inspect("")
-
-    Enum.map(Dense.ys(map), fn y ->
-      Enum.map(Dense.xs(map), fn x ->
-        case memo[{x, y}] do
-          nil -> "."
-          mapset -> mapset |> MapSet.size() |> Integer.to_string()
-        end
-      end)
-      |> Enum.join("")
-    end)
-    |> Enum.join("\n")
-    |> IO.puts()
   end
 end
