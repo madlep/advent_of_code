4e44541b0cc9e58b952b428abb9a44728c7fd87c
Julian Doherty <julian@juliandoherty.com>
2024-12-16 23:55:54 +1100

    day 15 part 2

--- 2024/elixir/lib/aoc24/day15.ex
+++ 2024/elixir/lib/aoc24/day15.ex
@@ -4,8 +4,13 @@ defmodule Aoc24.Day15 do
   alias Aoc24.Grid.Position
 
   @spec part1(String.t()) :: integer()
-  def part1(input) do
-    {map_robot, movements} = parse(input)
+  def part1(input), do: run(input, 1)
+
+  @spec part2(String.t()) :: integer()
+  def part2(input), do: run(input, 2)
+
+  defp run(input, scale_x) do
+    {map_robot, movements} = parse(input, scale_x)
 
     {map, _robot} =
       movements
@@ -15,14 +20,9 @@ defmodule Aoc24.Day15 do
 
     map
     |> Grid.positions()
-    |> Enum.filter(&(Grid.at(map, &1) == "O"))
+    |> Enum.filter(&(Grid.at(map, &1) in ["[", "O"]))
     |> Enum.map(&gps/1)
     |> Enum.sum()
-  end
-
-  @spec part2(String.t()) :: integer()
-  def part2(_input) do
-    -1
   end
 
   defp move_robot(robot, map, move) do
@@ -32,53 +32,80 @@ defmodule Aoc24.Day15 do
     end
   end
 
-  defp maybe_push_boxes(map, pos, move) do
+  defp maybe_push_boxes(map, {x, y} = pos, move) do
     move_contents = Grid.at(map, pos)
 
     case move_contents do
-      nil ->
-        {:ok, map}
+      nil -> {:ok, map}
+      "#" -> :blocked
+      "O" -> maybe_push_box(map, pos, move)
+      "[" -> maybe_push_wide_box(map, pos, {x + 1, y}, move)
+      "]" -> maybe_push_wide_box(map, {x - 1, y}, pos, move)
+    end
+  end
 
-      "#" ->
-        :blocked
+  defp maybe_push_box(map, pos, move) do
+    with moved_box_pos = move_pos(pos, move),
+         {:ok, map} <- maybe_push_boxes(map, moved_box_pos, move) do
+      {:ok, map |> Grid.move(pos, moved_box_pos)}
+    end
+  end
 
-      "O" ->
-        moved_box_pos = move_pos(pos, move)
+  defp maybe_push_wide_box(map, left_pos, right_pos, move = "<") do
+    with moved_left_pos = move_pos(left_pos, move),
+         moved_right_pos = move_pos(right_pos, move),
+         {:ok, map} <- maybe_push_boxes(map, moved_left_pos, move) do
+      {:ok, map |> Grid.move(left_pos, moved_left_pos) |> Grid.move(right_pos, moved_right_pos)}
+    end
+  end
 
-        case maybe_push_boxes(map, moved_box_pos, move) do
-          {:ok, new_map} -> {:ok, new_map |> Grid.move(pos, moved_box_pos)}
-          :blocked -> :blocked
-        end
+  defp maybe_push_wide_box(map, left_pos, right_pos, move = ">") do
+    with moved_left_pos = move_pos(left_pos, move),
+         moved_right_pos = move_pos(right_pos, move),
+         {:ok, map} <- maybe_push_boxes(map, moved_right_pos, move) do
+      {:ok, map |> Grid.move(right_pos, moved_right_pos) |> Grid.move(left_pos, moved_left_pos)}
+    end
+  end
+
+  defp maybe_push_wide_box(map, left_pos, right_pos, move) do
+    with moved_left_pos = move_pos(left_pos, move),
+         moved_right_pos = move_pos(right_pos, move),
+         {:ok, map} <- maybe_push_boxes(map, moved_left_pos, move),
+         {:ok, map} <- maybe_push_boxes(map, moved_right_pos, move) do
+      {:ok, map |> Grid.move(left_pos, moved_left_pos) |> Grid.move(right_pos, moved_right_pos)}
     end
   end
 
   defp move_pos(pos, move) do
     case move do
-      "^" ->
-        Position.up(pos)
-
-      "v" ->
-        Position.down(pos)
-
-      "<" ->
-        Position.left(pos)
-
-      ">" ->
-        Position.right(pos)
+      "^" -> Position.up(pos)
+      "v" -> Position.down(pos)
+      "<" -> Position.left(pos)
+      ">" -> Position.right(pos)
     end
   end
 
   defp gps({x, y}), do: x + y * 100
 
-  defp parse(input) do
+  defp parse(input, scale_x) do
     [map_input, movements_input] = String.split(input, "\n\n", parts: 2, trim: true)
-    {parse_map(map_input), parse_movements(movements_input)}
+    {parse_map2(map_input, scale_x), parse_movements(movements_input)}
   end
 
-  defp parse_map(input), do: sparse_grid(input, reduce: &grid_reducer/2)
+  defp parse_map2(input, 1), do: sparse_grid(input, reduce: {:raw, &reducer/2, nil})
+  defp parse_map2(input, 2), do: sparse_grid(input, reduce: {:raw, &reducer2/2, nil})
 
-  defp grid_reducer({robot_pos, "@"}, _acc), do: {:discard, robot_pos}
-  defp grid_reducer({_pos, char}, acc), do: {:keep, char, acc}
+  defp reducer({robot_pos, "@"}, {g, nil}), do: {g, robot_pos}
+  defp reducer({pos, "O"}, {g, acc}), do: {g |> Grid.put(pos, "O"), acc}
+  defp reducer({pos, "#"}, {g, acc}), do: {g |> Grid.put(pos, "#"), acc}
+
+  defp reducer2({{x, y}, "@"}, {g, nil}), do: {g, {x * 2, y}}
+
+  defp reducer2({{x, y}, "O"}, {g, acc}),
+    do: {g |> Grid.put({x * 2, y}, "[") |> Grid.put({x * 2 + 1, y}, "]"), acc}
+
+  defp reducer2({{x, y}, "#"}, {g, acc}),
+    do: {g |> Grid.put({x * 2, y}, "#") |> Grid.put({x * 2 + 1, y}, "#"), acc}
 
   defp parse_movements(input), do: input |> String.replace("\n", "") |> String.graphemes()
 end
