50e5090e35a1e904ce0c79301a6f1e1e50ce4c86
Julian Doherty <julian@juliandoherty.com>
2024-12-17 14:48:59 +1100

    tidy up

--- 2024/elixir/lib/aoc24/day16.ex
+++ 2024/elixir/lib/aoc24/day16.ex
@@ -1,73 +1,55 @@
 defmodule Aoc24.Day16 do
   import Aoc24.Parse
   alias Aoc24.Grid
-  import Aoc24.Grid.Position
+  alias Aoc24.Grid.Position
 
   @spec part1(String.t()) :: integer()
   def part1(input) do
-    {grid, {start_pos, end_pos}} = parse(input)
+    {g, {from, to}} = parse(input)
     dir = {1, 0}
 
-    {_, mincost, _} =
-      shortest(start_pos, dir, end_pos, grid, %{}, 0, 200_000, [start_pos], MapSet.new(), &>/2)
-
-    mincost
+    walk(from, dir, to, g, %{}, 0, 200_000, [from], MapSet.new(), &>/2)
+    |> elem(1)
   end
 
   @spec part2(String.t()) :: integer()
   def part2(input) do
-    {grid, {start_pos, end_pos}} = parse(input)
+    {g, {from, to}} = parse(input)
     dir = {1, 0}
 
-    {visited, mincost, _} =
-      shortest(start_pos, dir, end_pos, grid, %{}, 0, 200_000, [start_pos], MapSet.new(), &>/2)
+    {costs, min, _} =
+      walk(from, dir, to, g, %{}, 0, 200_000, [from], MapSet.new(), &>/2)
 
     {_, _, best} =
-      shortest(
-        start_pos,
-        dir,
-        end_pos,
-        grid,
-        visited,
-        0,
-        mincost + 1,
-        [start_pos],
-        MapSet.new(),
-        &>=/2
-      )
+      walk(from, dir, to, g, costs, 0, min + 1, [from], MapSet.new(), &>=/2)
 
     MapSet.size(best)
   end
 
-  defp shortest(_, _, _, _, visited, cost, mincost, _path, best, _) when cost > mincost,
-    do: {visited, mincost, best}
+  defp walk(_, _, _, _, costs, cost, min, _path, best, _) when cost > min, do: {costs, min, best}
 
-  defp shortest(pos, _, target, _, visited, cost, mincost, path, _best, _)
-       when pos == target and cost < mincost do
-    {visited, cost, MapSet.new([target | path])}
-  end
+  defp walk(p, _, to, _, costs, cost, min, path, _, _) when p == to and cost < min,
+    do: {costs, cost, MapSet.new([to | path])}
 
-  defp shortest(pos, _, target, _, visited, cost, mincost, path, best, _)
-       when pos == target and cost == mincost do
-    {visited, mincost, best |> MapSet.union(MapSet.new(path))}
-  end
+  defp walk(p, _, to, _, costs, cost, min, path, best, _) when p == to and cost == min,
+    do: {costs, min, best |> MapSet.union(MapSet.new(path))}
 
-  defp shortest(pos, dir, target, g, visited, cost, mincost, path, best, visited_check) do
-    with true <- visited_check.(visited[{pos, dir}], cost),
-         visited <- Map.put(visited, {pos, dir}, cost),
-         true <- Grid.at!(g, pos) != "#" do
-      path = [pos | path]
+  defp walk(p, dir, to, g, costs, cost, min, path, best, costs_check) do
+    with true <- costs_check.(costs[{p, dir}], cost),
+         costs <- Map.put(costs, {p, dir}, cost),
+         true <- Grid.at!(g, p) != "#" do
+      path = [p | path]
 
       [
-        {pos, rotate_right(dir), cost + 1000},
-        {pos, rotate_left(dir), cost + 1000},
-        {move(pos, dir), dir, cost + 1}
+        {p, Position.rotate_right(dir), cost + 1000},
+        {p, Position.rotate_left(dir), cost + 1000},
+        {Position.move(p, dir), dir, cost + 1}
       ]
-      |> Enum.reduce({visited, mincost, best}, fn {p, d, c}, {visited, mincost, best} ->
-        shortest(p, d, target, g, visited, c, mincost, path, best, visited_check)
+      |> Enum.reduce({costs, min, best}, fn {p, d, c}, {costs, min, best} ->
+        walk(p, d, to, g, costs, c, min, path, best, costs_check)
       end)
     else
-      _ -> {visited, mincost, best}
+      _ -> {costs, min, best}
     end
   end
 
@@ -76,7 +58,7 @@ defmodule Aoc24.Day16 do
     |> sparse_grid(reduce: {&parse_reducer/2, {nil, nil}})
   end
 
-  defp parse_reducer({start_pos, "S"}, {nil, end_pos}), do: {:discard, {start_pos, end_pos}}
-  defp parse_reducer({end_pos, "E"}, {start_pos, nil}), do: {:discard, {start_pos, end_pos}}
-  defp parse_reducer({_pos, "#"}, acc), do: {:keep, "#", acc}
+  defp parse_reducer({from, "S"}, {nil, to}), do: {:discard, {from, to}}
+  defp parse_reducer({to, "E"}, {from, nil}), do: {:discard, {from, to}}
+  defp parse_reducer({_, "#"}, acc), do: {:keep, "#", acc}
 end
