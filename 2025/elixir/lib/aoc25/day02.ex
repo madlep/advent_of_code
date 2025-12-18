8022c43c2b9bb141fc62ad5bd45c51de1ef498ef
Julian Doherty <julian@juliandoherty.com>
2025-12-18 18:08:09 +1100

    day 2 part 2

--- 2025/elixir/lib/aoc25/day02.ex
+++ 2025/elixir/lib/aoc25/day02.ex
@@ -1,47 +1,58 @@
 defmodule Aoc25.Day02 do
   @spec part1(String.t()) :: integer()
-  def part1(input) do
-    do_part1(input, 0)
-  end
+  def part1(input), do: sum_repeated_digits(input, &repeat_2/1, 0)
 
   @spec part2(String.t()) :: integer()
-  def part2(_input) do
-    -1
+  def part2(input), do: sum_repeated_digits(input, &repeat_any/1, 0)
+
+  def repeat_2(_n), do: [2]
+
+  def repeat_any(n) do
+    len_digits = n |> Integer.digits() |> length()
+    for i <- 2..len_digits//1, rem(len_digits, i) == 0, do: i
   end
 
-  defp do_part1("", acc), do: acc
+  defp sum_repeated_digits("", _factors_fn, acc), do: acc
 
-  defp do_part1(<<c::utf8, input::binary>>, acc) when c in [?,, ?\n], do: do_part1(input, acc)
+  defp sum_repeated_digits(<<c::utf8, input::binary>>, factors_fn, acc) when c in [?,, ?\n],
+    do: sum_repeated_digits(input, factors_fn, acc)
 
-  defp do_part1(input, acc) do
+  defp sum_repeated_digits(input, factors_fn, acc) do
     {num1, input} = Integer.parse(input)
     <<?-, input::binary>> = input
     {num2, input} = Integer.parse(input)
-    new_acc = acc + (invalid_nums(num1, num2) |> Enum.sum())
-    do_part1(input, new_acc)
+
+    new_acc = acc + (repeated_digits(num1, num2, factors_fn) |> Enum.sum())
+    sum_repeated_digits(input, factors_fn, new_acc)
   end
 
-  def to_split_digits(n) do
+  def repeated_digits(from, to, factors_fn) do
+    (factors_fn.(from) ++ factors_fn.(to))
+    |> Enum.uniq()
+    |> Stream.flat_map(fn repeat_count ->
+      from
+      |> to_split_digits(repeat_count)
+      |> Stream.unfold(fn split_digits ->
+        {
+          Integer.undigits(List.duplicate(split_digits, repeat_count) |> List.flatten()),
+          Integer.digits(Integer.undigits(split_digits) + 1)
+        }
+      end)
+      |> Stream.drop_while(&(&1 < from))
+      |> Stream.take_while(&(&1 <= to))
+    end)
+    |> Stream.uniq()
+  end
+
+  def to_split_digits(n, repeat_count) do
     digits = Integer.digits(n)
     len_digits = length(digits)
+    repeat_size = div(len_digits, repeat_count)
 
-    if rem(len_digits, 2) == 0 do
-      Enum.take(digits, div(len_digits, 2))
+    if rem(len_digits, repeat_count) == 0 do
+      Enum.take(digits, repeat_size)
     else
-      [1 | List.duplicate(0, div(len_digits, 2))]
+      [1 | List.duplicate(0, repeat_size)]
     end
-  end
-
-  def invalid_nums(from, to) do
-    from
-    |> to_split_digits()
-    |> Stream.unfold(fn split_digits ->
-      {
-        Integer.undigits(split_digits ++ split_digits),
-        Integer.digits(Integer.undigits(split_digits) + 1)
-      }
-    end)
-    |> Stream.drop_while(&(&1 < from))
-    |> Stream.take_while(&(&1 <= to))
   end
 end
--- 2025/elixir/test/aoc25/day02_test.exs
+++ 2025/elixir/test/aoc25/day02_test.exs
@@ -19,34 +19,65 @@ defmodule Aoc25.Day02Test do
     end
   end
 
-  describe "to_split_digits/2" do
+  describe "to_split_digits/3" do
     test "generates split for numbers with even length" do
-      assert Day02.to_split_digits(11) == [1]
-      assert Day02.to_split_digits(1234) == [1, 2]
-      assert Day02.to_split_digits(123_456) == [1, 2, 3]
+      assert Day02.to_split_digits(11, 2) == [1]
+      assert Day02.to_split_digits(1234, 2) == [1, 2]
+      assert Day02.to_split_digits(123_456, 2) == [1, 2, 3]
     end
 
     test "skips to next split for numbers with odd length" do
-      assert Day02.to_split_digits(1) == [1]
-      assert Day02.to_split_digits(123) == [1, 0]
-      assert Day02.to_split_digits(12345) == [1, 0, 0]
-      assert Day02.to_split_digits(1_234_567) == [1, 0, 0, 0]
+      assert Day02.to_split_digits(1, 2) == [1]
+      assert Day02.to_split_digits(123, 2) == [1, 0]
+      assert Day02.to_split_digits(12345, 2) == [1, 0, 0]
+      assert Day02.to_split_digits(1_234_567, 2) == [1, 0, 0, 0]
     end
   end
 
-  describe "invalid_nums/2" do
-    test "generates sequence from..to" do
-      assert Day02.invalid_nums(11, 22) |> Enum.to_list() == [11, 22]
-      assert Day02.invalid_nums(95, 115) |> Enum.to_list() == [99]
-      assert Day02.invalid_nums(998, 1012) |> Enum.to_list() == [1010]
-      assert Day02.invalid_nums(1_188_511_880, 1_188_511_890) |> Enum.to_list() == [1_188_511_885]
+  describe "repeated_digits/2" do
+    defmacro assert_repeat_2(from, to, expected) do
+      quote do
+        assert Day02.repeated_digits(unquote(from), unquote(to), &Day02.repeat_2/1)
+               |> Enum.to_list() == unquote(expected)
+      end
+    end
+
+    defmacro assert_repeat_any(from, to, expected) do
+      quote do
+        assert Day02.repeated_digits(unquote(from), unquote(to), &Day02.repeat_any/1)
+               |> Enum.to_list() == unquote(expected)
+      end
+    end
+
+    test "generates sequence from..to with 2 repeats" do
+      assert_repeat_2(11, 22, [11, 22])
+      assert_repeat_2(95, 115, [99])
+      assert_repeat_2(998, 1012, [1010])
+      assert_repeat_2(1_188_511_880, 1_188_511_890, [1_188_511_885])
+      assert_repeat_2(222_220, 222_224, [222_222])
+      assert_repeat_2(1_698_522, 1_698_528, [])
+      assert_repeat_2(446_443, 446_449, [446_446])
+      assert_repeat_2(38_593_856, 38_593_862, [38_593_859])
+    end
+
+    test "generates sequence from..to with any number of repeats" do
+      assert_repeat_any(11, 22, [11, 22])
+      assert_repeat_any(95, 115, [99, 111])
+      assert_repeat_any(998, 1012, [999, 1010])
+      assert_repeat_any(1_188_511_880, 1_188_511_890, [1_188_511_885])
+      assert_repeat_any(222_220, 222_224, [222_222])
+      assert_repeat_any(1_698_522, 1_698_528, [])
+      assert_repeat_any(446_443, 446_449, [446_446])
+      assert_repeat_any(38_593_856, 38_593_862, [38_593_859])
+      assert_repeat_any(565_653, 565_659, [565_656])
+      assert_repeat_any(824_824_821, 824_824_827, [824_824_824])
+      assert_repeat_any(2_121_212_118, 2_121_212_124, [2_121_212_121])
     end
   end
 
   describe "part 2" do
-    @tag skip: "pending"
     test "part 2", ctx do
-      assert Day02.part2(ctx.example) == :implement_me
+      assert Day02.part2(ctx.example) == 4_174_379_265
     end
   end
 end
