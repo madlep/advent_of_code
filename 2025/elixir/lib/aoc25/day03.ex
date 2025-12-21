39e6545ade8e7f58618f0c88ab55123cee1364a4
Julian Doherty <julian@juliandoherty.com>
2025-12-21 16:23:20 +1100

    day 3 part 2

--- 2025/elixir/lib/aoc25/day03.ex
+++ 2025/elixir/lib/aoc25/day03.ex
@@ -1,27 +1,27 @@
 defmodule Aoc25.Day03 do
   @spec part1(String.t()) :: integer()
-  def part1(input) do
+  def part1(input), do: run(input, 2)
+
+  @spec part2(String.t()) :: integer()
+  def part2(input), do: run(input, 12)
+
+  defp run(input, batteries) do
     input
     |> String.split("\n", trim: true)
     |> Stream.map(&parse/1)
-    |> Stream.map(&largest_jolt/1)
+    |> Stream.map(&largest_jolt(&1, batteries))
     |> Enum.sum()
-  end
-
-  @spec part2(String.t()) :: integer()
-  def part2(_input) do
-    -1
   end
 
   defp parse(line), do: line |> String.to_integer() |> Integer.digits()
 
-  def largest_jolt(bank) do
-    # n1 can't be last
-    n1 = bank |> Enum.drop(-1) |> Enum.max()
+  def largest_jolt(bank, batteries, acc \\ [])
+  def largest_jolt(_bank, 0, acc), do: acc |> Enum.reverse() |> Integer.undigits()
+
+  def largest_jolt(bank, batteries, acc) do
+    n1 = bank |> Enum.drop(1 - batteries) |> Enum.max()
 
     {_bank_head, [^n1 | bank_rest]} = Enum.split_while(bank, &(&1 < n1))
-    n2 = Enum.max(bank_rest)
-
-    n1 * 10 + n2
+    largest_jolt(bank_rest, batteries - 1, [n1 | acc])
   end
 end
--- 2025/elixir/test/aoc25/day03_test.exs
+++ 2025/elixir/test/aoc25/day03_test.exs
@@ -23,18 +23,31 @@ defmodule Aoc25.Day03Test do
   end
 
   describe "part 2" do
-    @tag skip: "pending"
     test "part 2", ctx do
-      assert Day03.part2(ctx.example) == -2
+      assert Day03.part2(ctx.example) == 3_121_910_778_619
     end
   end
 
-  describe "largest_jolt/1" do
+  describe "largest_jolt/2" do
+    defmacrop assert_largest_jolt(bank, batteries, expected) do
+      quote do
+        assert unquote(bank) |> Integer.digits() |> Day03.largest_jolt(unquote(batteries)) ==
+                 unquote(expected)
+      end
+    end
+
     test "calculates largest jolt turning on 2 batteries" do
-      assert 987_654_321_111_111 |> Integer.digits() |> Aoc25.Day03.largest_jolt() == 98
-      assert 811_111_111_111_119 |> Integer.digits() |> Aoc25.Day03.largest_jolt() == 89
-      assert 234_234_234_234_278 |> Integer.digits() |> Aoc25.Day03.largest_jolt() == 78
-      assert 818_181_911_112_111 |> Integer.digits() |> Aoc25.Day03.largest_jolt() == 92
+      assert_largest_jolt(987_654_321_111_111, 2, 98)
+      assert_largest_jolt(811_111_111_111_119, 2, 89)
+      assert_largest_jolt(234_234_234_234_278, 2, 78)
+      assert_largest_jolt(818_181_911_112_111, 2, 92)
+    end
+
+    test "calcualtes largest jolt turning on 12 batteries" do
+      assert_largest_jolt(987_654_321_111_111, 12, 987_654_321_111)
+      assert_largest_jolt(811_111_111_111_119, 12, 811_111_111_119)
+      assert_largest_jolt(234_234_234_234_278, 12, 434_234_234_278)
+      assert_largest_jolt(818_181_911_112_111, 12, 888_911_112_111)
     end
   end
 end
